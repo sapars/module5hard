05b6fa39aaa31ab64064534f0293c1fae3e5296a
sapars <unstd@ya.ru>
2024-06-26 00:11:04 +0300

    M5H 2.0 Прошли тесты

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from user import User
 from video import Video
 from tube import UrTube
-from notes_v import *
 
 
-# def add_video(*movs: tuple):
-#     videos: list[Video] = []
-#     for i in movs:
-#         videos.append(Video(*i))
-#     return videos
-#
-# videos = add_video(*movs)
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 5, adult_mode=True)
 
+ur.add_video(v1, v2)
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
 
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register_new_user('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register_new_user('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
 
-tube: UrTube = UrTube()
-tube.add_video(*movs)
-print(tube.get_videos('z'))
-tube.watch_video("ZZZ")
-#
-# print(tube.video_titles)
-# # tube.register_new_user(user1)
-# print(tube)
+# Проверка входа в другой аккаунт
+ur.register_new_user('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
